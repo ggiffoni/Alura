142500f244b150b814e5f7c2e8f660f99ad15ce1
ggiffoni <89454421+ggiffoni@users.noreply.github.com>
2021-10-26 12:16:04 -0300

    novoex

---
+++ ex1.js
@@ -0,0 +1,11 @@
+var idade = 12
+var nome = "Júlia "
+var sobrenome = "Lemos"
+var dezanosdepois = 10
+var bacharel = false
+const nomecompleto = nome + sobrenome
+const resultado = idade + dezanosdepois 
+console.log(nomecompleto);
+console.log(resultado);
+console.log(bacharel == 0);
+console.log(bacharel == 1);
