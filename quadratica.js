a71398b7a891ff7cfd42df3373543d7cbd9efd3f
ggiffoni <89454421+ggiffoni@users.noreply.github.com>
2021-10-30 10:33:08 -0300

    projeto cúbica

---
+++ quadratica.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = -2;
+let c = -3;
+
+let delta = (b * b - ( 4 * a * c ) );
+
+let raizdelta = Math.pow(delta, 0.5);
+
+let x1 = (-b + raizdelta) / 2 * a;
+let x2 = (-b - raizdelta) / 2 * a;
+
+
+
+console.log(delta);
+console.log(x1);
+console.log(x2);
